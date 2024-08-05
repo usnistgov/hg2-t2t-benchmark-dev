e08934bd2ecff0ec994b3333bdd5d44fbcfd05d6
ndwarshuis <ndwar@yavin4.ch>
2024-08-05 14:37:31 -0400

    FIX sort beds

--- pipelines/bench-compare/workflow/scripts/R/format.R
+++ pipelines/bench-compare/workflow/scripts/R/format.R
@@ -3,15 +3,15 @@ library(tidyverse)
 # dissimilarity = (indels + mismatches)/(projection block length) * 100
 # TODO semicolon was a bad choice of separator since INFO uses it
 
-## chr2int <- function(s) {
-##   .s <- str_extract(s, "chr([0-9XY]+)_?.*", group = 1)
-##   if_else(.s == "X", 23, if_else(.s == "Y", 24, suppressWarnings(as.numeric(.s))))
-## }
+chr2int <- function(s) {
+  .s <- str_extract(s, "chr([0-9XY]+)_?.*", group = 1)
+  if_else(.s == "X", 23, if_else(.s == "Y", 24, suppressWarnings(as.numeric(.s))))
+}
 
-## int2chr <- function(i, suffix) {
-##   c <- if_else(i == 23, "X", if_else(i == 24, "Y", as.character(i)))
-##   sprintf("chr%s_%s", c, suffix)
-## }
+int2chr <- function(i, suffix) {
+  c <- if_else(i == 23, "X", if_else(i == 24, "Y", as.character(i)))
+  sprintf("chr%s_%s", c, suffix)
+}
 
 ## data_fmt <- c("REF", "ALT", "dissimilarity", "regions", "query_phase", "bench_phase") %>%
 ##   sprintf("%s=%%s", .) %>%
@@ -19,13 +19,13 @@ library(tidyverse)
 
 path <- snakemake@input[[1]]
 
-## chrom_suffix <- if_else(str_detect(path, "pat"), "PATERNAL", "MATERNAL")
+chrom_suffix <- if_else(str_detect(path, "pat"), "PATERNAL", "MATERNAL")
 
 readr::read_tsv(path,
                 col_types = "ciidc",
                 col_names = c("chrom", "start", "end", "dissimilarity", "data")
                 ) %>%
-  ## mutate(chrom = chr2int(chrom)) %>%
+  mutate(chrom = chr2int(chrom)) %>%
   separate_longer_delim(data, "~") %>%
   separate_wider_delim(data,
                        delim = ";",
@@ -41,6 +41,6 @@ readr::read_tsv(path,
   ## ) %>%
   ## filter(str_detect(regions, "notinsegdup")) %>%
   ## select(chrom, start, end, data) %>%
-  ## arrange(chrom, start, end) %>%
-  ## mutate(chrom = int2chr(chrom, chrom_suffix)) %>%
+  arrange(chrom, start, end) %>%
+  mutate(chrom = int2chr(chrom, chrom_suffix)) %>%
   readr::write_tsv(snakemake@output[[1]], col_names = FALSE)
